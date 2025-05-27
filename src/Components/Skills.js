4474f95f0c8d2553bc0a81d65652db0f5352f810
RaineJacob <119743501+RaineJacob@users.noreply.github.com>
2025-05-27 12:59:53 +0530

    Update Skills.js

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,31 +1,39 @@
-import React from 'react'; 
-import { CgCPlusPlus } from "react-icons/cg";
-import {FaReact, FaPython, FaGitAlt, FaGithub, FaNpm, FaFigma, FaBootstrap} from "react-icons/fa";
-import {DiNodejs, DiJavascript1} from "react-icons/di";
-import {SiExpress, SiMongodb, SiPostman, SiVercel} from "react-icons/si";
+import React from 'react';
+import { FaPython, FaGitAlt, FaGithub, FaFigma } from "react-icons/fa";
+import { 
+  SiVercel, 
+  SiMysql, 
+  SiPowerbi, 
+  SiJupyter, 
+  SiPostgresql, 
+  SiTableau, 
+  SiMicrosoftoffice, 
+  SiAmazonaws 
+} from "react-icons/si";
+import { AiOutlineQuestionCircle } from "react-icons/ai"; // fallback
 
-const Skills = ({skill}) => {
+const Skills = ({ skill }) => {
     const icon = {
-        'C++': <CgCPlusPlus/>,
-        Postman: <SiPostman/>,
-        React: <FaReact/>,
-        Javascript: <DiJavascript1/>,
-        Node : <DiNodejs/>,
-        Express : <SiExpress/>,
-        MongoDb : <SiMongodb/>,
-        Git : <FaGitAlt/>,
-        Github : <FaGithub/>,
-        Npm : <FaNpm/>,
-        Figma : <FaFigma/>,
-        Bootstrap: <FaBootstrap/>,
-        Vercel : <SiVercel/>
-    }
-    
-  return (
-    <div title={skill} className='SkillBox'>
-      {icon[skill]}
-    </div>
-  )
-}
+        Python: <FaPython />,
+        Jupyter: <SiJupyter />,
+        MYSQL: <SiMysql />,
+        Postgres: <SiPostgresql />,
+        Git: <FaGitAlt />,
+        Github: <FaGithub />,
+        PowerBI: <SiPowerbi />,
+        Tableau: <SiTableau />,
+        Excel: <SiMicrosoftoffice />,
+        'MS Office': <SiMicrosoftoffice />,
+        Figma: <FaFigma />,
+        Vercel: <SiVercel />,
+        AWS: <SiAmazonaws />
+    };
 
-export default Skills
+    return (
+        <div title={skill} className='SkillBox'>
+            {icon[skill] || <AiOutlineQuestionCircle />}
+        </div>
+    );
+};
+
+export default Skills;
