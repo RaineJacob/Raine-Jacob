01fe8d74f6326a93de49c3dfef81a14d3650a66a
RaineJacob <119743501+RaineJacob@users.noreply.github.com>
2025-05-27 13:13:24 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
 
       <div className='footerLinks'>
         <a href="https://github.com/RaineJacob" target='_blank'><FaGithub/></a>
-        <a href="www.linkedin.com/in/rainejacob" target='_blank'><FaLinkedin/></a>
+        <a href="https://www.linkedin.com/in/raine-jacob-1803b0259/" target='_blank'><FaLinkedin/></a>
         <a href='mailTo:raine.jacob07@gmail.com' target='_blank'><GrMail/></a>
       </div>
     </footer>
