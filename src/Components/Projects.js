79ef5dd8462b4878765883bca3f8eff9a44c0a12
RaineJacob <119743501+RaineJacob@users.noreply.github.com>
2025-06-13 10:48:48 +0530

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,6 +4,7 @@ import HotelBookingImage from '../images/HotelBookingImage.png';
 import DeliveryDelayImage from '../images/DeliveryDelayImage.png';
 import MarketingAnalysisImage from '../images/MarketingAnalysisImage.png';
 import HrAnalyticsImage from '../images/HrAnalyticsImage.png';
+import OlistImage from '../images/Olist.jpg';
 
 const Projects = () => {
   return (
@@ -14,6 +15,7 @@ const Projects = () => {
         <ProjectBox projectPhoto={DeliveryDelayImage} projectName="Delivery Delay Analysis" />
         <ProjectBox projectPhoto={MarketingAnalysisImage} projectName="Marketing Analysis" />
         <ProjectBox projectPhoto={HrAnalyticsImage} projectName="Hr Analytics" />
+        <ProjectBox projectPhoto={OlistImage} projectName="Olist Ecommerce Analysis" />
       </div>
 
     </div>
