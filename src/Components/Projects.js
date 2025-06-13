21db942d3ed0ec2fff22db29400253afc5b96f62
RaineJacob <119743501+RaineJacob@users.noreply.github.com>
2025-06-13 10:50:34 +0530

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,7 +4,7 @@ import HotelBookingImage from '../images/HotelBookingImage.png';
 import DeliveryDelayImage from '../images/DeliveryDelayImage.png';
 import MarketingAnalysisImage from '../images/MarketingAnalysisImage.png';
 import HrAnalyticsImage from '../images/HrAnalyticsImage.png';
-import OlistImage from '../images/Olist.jpg';
+import OlistImage from '../images/olist.jpg';
 
 const Projects = () => {
   return (
